634e0e78408041d96b0801ea971fe0796848bb21
Theodore Wu <twu00@stuy.edu>
2019-01-03 21:14:18 -0500

    Made the basic layout for MapGen, figuring out how lanterna works in Game.java

--- Game.java
+++ Game.java
@@ -1,1 +1,15 @@
-
+import com.googlecode.lanterna.TerminalFacade;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.terminal.Terminal;
+// javac -cp "lanterna.jar;." Game.java && java -cp "lanterna.jar;." Game
+public class Game{
+	public static void main(String[] args) throws InterruptedException{
+		Screen screen = TerminalFacade.createScreen();
+		screen.startScreen();
+		screen.putString(10,5,"hello world", Terminal.Color.WHITE, Terminal.Color.BLACK);
+		screen.refresh();
+		Thread.sleep(300000);
+		screen.stopScreen();
+		System.exit(1);
+	}
+}
--- MapGen.java
+++ MapGen.java
@@ -1,1 +1,4 @@
-
+public class MapGen{
+	private char[][] map;
+	MapGen(int width, int length, Room[] rooms){};
+}
