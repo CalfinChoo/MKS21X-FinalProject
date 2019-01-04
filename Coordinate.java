77f4907ecbf188bfdd7cd7f5ec4b0dc0d4980772
Theodore Wu <twu00@stuy.edu>
2019-01-03 21:17:30 -0500

    made the coordinate class to record enemy and player positions

--- Coordinate.java
+++ Coordinate.java
@@ -1,3 +1,15 @@
 public class Coordinate{
-  private int x,y;
+	private int x,y
+	public getX(){
+		return x;
+	}
+	public setX(int n){
+		x = n;
+	}
+	public getY(){
+		return y;
+	}
+	public setY(int n){
+		y = n;
+	}
 }
