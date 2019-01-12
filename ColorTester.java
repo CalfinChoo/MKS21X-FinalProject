b2f187a835abfdc31f3664133eafaed192785aba
Theodore Wu <twu00@stuy.edu>
2019-01-12 00:35:53 -0500

    converted to lanterna 3.0 allowing for better colors and such.

--- ColorTester.java
+++ ColorTester.java
@@ -3,12 +3,21 @@ import com.googlecode.lanterna.input.*;
 import com.googlecode.lanterna.terminal.*;
 import com.googlecode.lanterna.screen.*;
 import java.io.IOException;
-import java.awt.Color;
 public class ColorTester{
+	private static void putScreen(Screen screen, int row, int width, String message, TextColor fore, TextColor back){
+		for (int x = 0; x < message.length(); x++){
+			screen.setCharacter(row+x,width, new TextCharacter(message.charAt(x), fore, back));
+		}
+	}
+	private static void putScreenV(Screen screen, int row, int width, String message, TextColor fore, TextColor back){
+		for (int x = 0; x < message.length(); x++){
+			screen.setCharacter(row,width+x, new TextCharacter(message.charAt(x), fore, back));
+		}
+	}
 	// input the character type then the R, G, then B color values (0 <= RBG <= 255)
 	public static void main(String[] args)throws InterruptedException, IOException{
 		Screen screen = new DefaultTerminalFactory().createScreen();
-		System.out.println(args[3]);
+		//System.out.println(args[3]);
 		TextCharacter chr = new TextCharacter(args[0].charAt(0), 
 			new TextColor.RGB(Integer.parseInt(args[1]), Integer.parseInt(args[2]), Integer.parseInt(args[3])), 
 			TextColor.ANSI.DEFAULT
@@ -16,6 +25,7 @@ public class ColorTester{
 
 		screen.startScreen();
 		screen.setCharacter(1,1, chr);
+		putScreen(screen,2,2, "hello", TextColor.ANSI.BLACK, TextColor.ANSI.WHITE);
 		screen.refresh();
 		Thread.sleep(1000);
 		screen.close();
--- MapGen.java
+++ MapGen.java
@@ -1,60 +1,30 @@
-import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.TextColor;
 public class MapGen{
 	private int width, height;
-	private String[][] map; //player object and enemy map
-	private Terminal.Color[][] colorMap;
-	private Terminal.Color[][] backColorMap;
+	private TextCharacter [][] map;
 
 	MapGen(int width, int height, int vWidth, int vHeight){
-		map = createMap(width,height);
-		colorMap = new Terminal.Color[height][width];
-		backColorMap = new Terminal.Color[height][width];
-		for (int h = 0; h < height; h++){
-			for (int w = 0; w<width;w++){
-				colorMap[h][w] = Terminal.Color.BLUE;
-				backColorMap[h][w] = Terminal.Color.BLACK;
-			}
-		}
+		map = new TextCharacter[height][width];
 		this.width = width; this.height = height;
+		map = createMap(width,height);
 		addBorder(vWidth, vHeight);
 	}
 	MapGen(int width, int height){
-		map = new String[height][width];
-		colorMap = new Terminal.Color[height][width];
-		backColorMap = new Terminal.Color[height][width];
-		for (int h = 0; h < height; h++){
-			for (int w = 0; w<width;w++){
-				colorMap[h][w] = Terminal.Color.BLUE;
-				backColorMap[h][w] = Terminal.Color.BLACK;
-			}
-		}
+		map = new TextCharacter[height][width];		
 		this.width = width; this.height = height;
+		map = createMap(width,height);
 	}
-	public String[][] getMap(){
+	public TextCharacter[][] getMap(){
 		return map;
 	}
-	public Terminal.Color[][] getCM(){
-		return colorMap;
-	}
-	public Terminal.Color[][] getBCM(){
-		return backColorMap;
-	}
 
-	public String getMapP(int x, int y){
+	public TextCharacter getMapP(int x, int y){
 		return map[y][x];
 	}
-	public Terminal.Color getCMP(int x, int y){
-		return colorMap[y][x];
-	}
 
-	public void setMap(int x, int y, String value){
+	public void setMap(int x, int y, TextCharacter value){
 		map[y][x] = value;
-	}
-	public void setCM(int x, int y, Terminal.Color color){
-		colorMap[y][x] = color;
-	}
-	public void setBCM(int x, int y, Terminal.Color color){
-		backColorMap[y][x] = color;
 	}
 
 	public int getWidth(){
@@ -64,17 +34,16 @@ public class MapGen{
 		return height;
 	}
 
-	private static String[][] createMap(int width, int height, Room[] rooms){
+	private static TextCharacter[][] createMap(int width, int height, Room[] rooms){
 		return null;
 	};
-	public static String[][] createMap(int width, int height){
+	public static TextCharacter[][] createMap(int width, int height){
 		// for testing, fills the string with empty stuff
-		String[][] out = new String[height][width];
+		TextCharacter[][] out = new TextCharacter[height][width];
 		int i = 0;
 		for (int h = 0; h < height; h++){
 			for (int w = 0; w<width;w++){
-				if (w == 0){out[h][w] = "" + i++ %10;}
-				else {out[h][w] = "O";}
+				out[h][w] = new TextCharacter('O', TextColor.ANSI.CYAN, TextColor.ANSI.CYAN);
 			}
 		}
 		return out;
@@ -83,9 +52,7 @@ public class MapGen{
 		for(int y = 0; y < height;y++){
 			for (int x = 0; x < width; x++){
 				if (x < (vWidth-1)/2 || y < (vHeight-1)/2 || x > width - (vWidth-1)/2 || y > height - (vHeight-1)/2){
-					//if (y < (vHeight-1)/2) System.out.println("("+x+","+y+")");
-					map[y][x] = "@";
-					colorMap[y][x] = Terminal.Color.RED;
+					map[y][x] = new TextCharacter('@', TextColor.ANSI.RED, TextColor.ANSI.DEFAULT);
 				}
 			}
 		}
